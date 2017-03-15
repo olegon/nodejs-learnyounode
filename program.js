5ccc332cf249a7c392e20b1e21c015b5d0c7904a
Leandro Oliveira <olegon.main@gmail.com>
2017-03-14 23:19:29 -0300

    HTTP COLLECT

--- program.js
+++ program.js
@@ -3,9 +3,18 @@ const http = require('http')
 const [,,url] = process.argv
 
 http.get(url, (response) => {
+    let text = ''
+
     response.setEncoding('utf8')
 
-    response.on('data', console.log)
+    response.on('data', chunk => {
+        text += chunk
+    })
+
+    response.on('end', () => {
+        console.log(text.length)
+        console.log(text)
+    })
 
     response.on('error', console.error)
 })
