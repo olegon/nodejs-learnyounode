d5c133f41523f70014bc81c4a9658deebbdc36b6
Leandro Oliveira <olegon.main@gmail.com>
2017-03-15 00:02:03 -0300

    HTTP FILE SERVER

--- program.js
+++ program.js
@@ -1,28 +1,14 @@
-const net = require('net')
+const http = require('http')
+const fs = require('fs')
 
-const [,,port] = process.argv
+const [,,port,filename] = process.argv
 
-const server = net.createServer(socket => {
-    socket.end(formatDate(new Date()))
-})
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'content-type': 'text/plain'
+    })
 
-function formatDate(date) {
-    const day = date.getDate()
-    const month = date.getMonth() + 1
-    const year = date.getFullYear()
-
-    const hour = date.getHours()
-    const minute = date.getMinutes()
-
-    return `${year}-${padleft(month, '0', 2)}-${padleft(day, '0', 2)} ${padleft(hour, '0', 2)}:${padleft(minute, '0', 2)}\n`
-}
-
-function padleft(str, char, size) {
-    let result = str.toString()
-
-    while (result.length < size) result = char + result
-
-    return result
-}
+    fs.createReadStream(filename).pipe(res)
+});
 
 server.listen(Number(port))
