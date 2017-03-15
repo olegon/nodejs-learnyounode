0c7f85fb005bf6b0e7835d31a8b7bed962576be4
Leandro Gonçalves de Oliveira <olegon.main@gmain.com.br>
2017-03-15 09:30:50 -0300

    HTTP UPPERCASE

--- program.js
+++ program.js
@@ -1,14 +1,36 @@
 const http = require('http')
 const fs = require('fs')
 
-const [,,port,filename] = process.argv
+const [,,port] = process.argv
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'content-type': 'text/plain'
+    if (req.method !== 'POST') return res.end('Send me a POST!')
+
+    const chunks = []
+
+    req.on('data', chunk => {
+        chunks.push(chunk)
     })
 
-    fs.createReadStream(filename).pipe(res)
+    req.on('end', () => {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        })
+
+        res.end(chunks.map(chunk => chunk.toString()).join('').toUpperCase())
+    })
 });
 
 server.listen(Number(port))
+
+/*
+
+    Dica: poderia ser usado o módulo through2-map.
+
+    const map = require('through2-map')
+    { ... }
+    req
+    .pipe(map(chunk => chunk.toString().toUpperCase()))
+    .pipe(res)
+
+*/
