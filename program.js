3e0903c9836584b8f03e086dc9676f4cd46aa691
Leandro Gonçalves de Oliveira <olegon.main@gmain.com.br>
2017-03-15 09:45:11 -0300

    HTTP JSON API SERVER

--- program.js
+++ program.js
@@ -1,36 +1,46 @@
 const http = require('http')
-const fs = require('fs')
+const url = require('url')
 
 const [,,port] = process.argv
 
 const server = http.createServer((req, res) => {
-    if (req.method !== 'POST') return res.end('Send me a POST!')
+    /* 2nd parameter: parseQueryString = true */
+    const parsedUrl = url.parse(req.url, true)
 
-    const chunks = []
+    if (parsedUrl.pathname === '/api/parsetime') {
+        const isoString = parsedUrl.query.iso
 
-    req.on('data', chunk => {
-        chunks.push(chunk)
-    })
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(dateObject(isoString), null, 4))
+    }
+    else if (parsedUrl.pathname === '/api/unixtime') {
+        const isoString = parsedUrl.query.iso
 
-    req.on('end', () => {
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        })
-
-        res.end(chunks.map(chunk => chunk.toString()).join('').toUpperCase())
-    })
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(dateUnix(isoString), null, 4))
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.end('Available endpoints: /api/parsetime and /api/unixtime')
+    }    
 });
 
+function dateObject(isoString) {
+    const date = new Date(isoString)
+
+    return {
+        'hour': date.getHours(),
+        'minute': date.getMinutes(),
+        'second': date.getSeconds()
+    }
+}
+
+function dateUnix(isoString) {
+    const date = new Date(isoString)
+
+    return {
+        'unixtime': date.getTime()
+    }
+}
+
 server.listen(Number(port))
-
-/*
-
-    Dica: poderia ser usado o módulo through2-map.
-
-    const map = require('through2-map')
-    { ... }
-    req
-    .pipe(map(chunk => chunk.toString().toUpperCase()))
-    .pipe(res)
-
-*/
